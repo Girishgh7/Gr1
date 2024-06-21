321cbbfb8d7da5a97d3bb5c88f41c9958cc2b18d
🐒Girish🤖 <115522904+Girishgh7@users.noreply.github.com>
2024-06-21 12:19:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import tensorflow as tf 
+import gradio as gr 
+
+model=tf.keras.models.load_model('model.h5')
+
+def recognize_digit(image):
+    if image is not None:
+        image=image.reshape((1,28,28,1)).astype('float32')/255
+        
+        prediction =model.predict(image)
+        
+        return{str(i):float(prediction[0][i]) for i in range (10)}
+    else:
+        return ''
+    
+
+iface=gr.Interface(
+    fn=recognize_digit,
+    input=gr.Image(shape=(28,28),image_mode='L',invert_colors='True',source='canvas'),
+    outputs=gr.Label(num_top_classes=3),
+    live=True
+    
+)  
+
+iface.launch()  
